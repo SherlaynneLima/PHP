da372037f5a46c526b744ebd0a581088c3fcc64f
sherlaynne.lima <sherlaynnelima@hotmail.com>
2026-06-20 18:23:39 -0300

    teste

--- Aula 1 - Banco de dados/index.php
+++ Aula 1 - Banco de dados/index.php
@@ -2,6 +2,6 @@
 	//Conectando ao banco de dados link: localhost/phpmyadmin
 	//dbname = ao nome do banco que eu criei
 	// root e '' é usuário e senha do banco que vem por padrão essa
-	$pdo = new PDO('mysql:host=localhost;dbname=modulo_7','root','');
+	$pdo = new PDO('mysql:host=localhost;dbname=BDteste','root','');
 
 ?>
